813c0d4edcc1d8ec33096b0ce3f798c42aed61c9
nicocaporohde <nicocaporohde@gmail.com>
2021-03-19 13:52:11 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t8iUjDnMahUv9Q27_3qcuKMx3ypBvQHe7_LCFpL6DcY/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAYdIOqDtY1NkFOVghIeYUmxTXzqPaSLwA';
+var googleApiKey = 'AIzaSyCeo3IKQ5Wu8cO8YhUQoX45awrR3EWJPQs';
